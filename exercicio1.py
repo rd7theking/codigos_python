bf2e9f8bbde22dcd330ada299a873a91d5ea2c0f
ryan_pontes <ryanchristianpontessilva@gmail.com>
2024-09-03 17:15:29 -0300

    questão 1 e questão 3

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+idade_anos = float(input("Digite a idade em anos: "))
+idade_meses = idade_anos*12
+idade_dias = idade_anos*365
+
+print(f"A idade em meses é: {idade_meses}")
+print(f"A idade em dias é: {idade_dias}")
---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+nome_fazenda = input("Digite o nome da fazenda: ")
+cavalos = float(input("Digite a quantidade de cavalos: "))
+ferraduras = cavalos*4
+
+print(f"A fazenda {nome_fazenda} possui {cavalos} cavalos, a quantidade total de ferraduras dessa fazenda é: {ferraduras}")
