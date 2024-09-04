94ef73c212ee807b15a7de464de4c944779ceca5
ryan_pontes <ryanchristianpontessilva@gmail.com>
2024-09-04 14:25:05 -0300

    Trabalhando com estrutura condicional

---
+++ exemplo-condicional.py
@@ -0,0 +1,9 @@
+valor = int(input("informe um valor numérico: "))
+
+if valor>20:
+    print(f"o numéro {valor} é maior ou igual a 20 \n")
+elif valor>=10 and valor <=20:
+    print(f"o numéro {valor} está entre 10 e 20 \n")
+
+else:
+    print(f"o numéro {valor} é menor que 20 \n")
